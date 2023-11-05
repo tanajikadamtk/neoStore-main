fd9f90db8f5428ed0cfefd94f1ad7736798177b0
tanajikadamtk <tanajikadam5298@gmail.com>
2023-11-05 18:12:15 +0530

    update css and file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import "./App.css";
-// import "./sample.css";
+// import "./App.css";
+import "./sample.css";
 // import "../src/assets/scss/style.css"
 
 // neoStore-main\src\assets\scss\style.css
